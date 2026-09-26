2fa0477a757cb0cfa34d03954d5ae44562ead229
Zac Lloyd-Humphreys <zaclloyd01@proton.me>
2026-09-26 20:46:09 +0200

    further cleanup of main
    the dict loop means i need basically no code atm!

--- main.py
+++ main.py
@@ -20,15 +20,6 @@ def main():
     print(type(response.answers["is_solicitation"]))
     print(response.answers["is_solicitation"])
     print(response.answers["not_valid_email_domain"])
-
-    is_name_enumerated_ans = response.nouls["is_name_enumerated"].noul 
-    not_valid_email_domain = response.nouls["not_valid_email_domain"].noul 
-    is_temporal_mismatch  = response.nouls["is_temporal_mismatch"].noul
-    is_not_dutch_english = response.nouls["is_not_dutch/english"].noul
-    contains_weird_attachments = response.nouls["contains_weird_attachments"].noul 
-    is_payment = response.nouls["is_payment"].noul 
-    is_solicitation = response.nouls["is_solicitation"].noul 
-
     # could actually iterate through the dict instead of doing all these if statments
     # for answer in response.nouls.noul
     for answer in response.nouls:
